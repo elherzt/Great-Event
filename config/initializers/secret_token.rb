30b4f2ffa930f519c0ea91ab5bd9614377be6a4d
Jesus Romero <elherzt@gmail.com>
2013-11-25 15:15:11 -0600

    created application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GreatEvent::Application.config.secret_key_base = '5282d75af30b32f9baf9fcb67e1a1fdc352bbe853d519bbed95fc146da19ebfe9177c57f28156032f41e51b5817114d95f71b33360100e2d65f442fffc56ffd4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GreatEvent::Application.config.session_store :cookie_store, key: '_great_event_session'
