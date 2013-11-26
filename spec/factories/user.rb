65b61511d791033ee75244f8c0e32de624b5d6e3
Jesus Romero <elherzt@gmail.com>
2013-11-26 16:36:41 -0600

    refactor factory to user usign faker

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,9 +1,9 @@
 require 'faker'
 FactoryGirl.define do
   factory :user do
-    name "Dummy User Test2"
-    username "dummy_user3"
-    email "usertest3@test.com"
+    name Faker::Name.name
+    username Faker::Internet.user_name
+    email Faker::Internet.email
     password "12345678"
     password_confirmation "12345678"
   end
