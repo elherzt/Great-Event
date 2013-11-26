35723990a1a9d4a6024973a3c69adc52fd4d960e
Jesus Romero <elherzt@gmail.com>
2013-11-26 16:37:36 -0600

    refactored tests

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -3,13 +3,8 @@ require "spec_helper"
 describe "user sign in" do
     let (:user) {create(:user)} 
   it "allows users to sign in" do
-    visit '/users/sign_in'
-
-    fill_in "user[username]",    :with => user.username
-    fill_in "user[password]", :with => user.password
-
-    click_button "Sign in"
-
+    login_as(user, :scope => :user, :run_callbacks => false)
+    visit user_path user
     page.should have_content(user.username)
   end
 end
