e51ba5a3055fb08f3be80364c53e1def1d3a64c3
Jesus Romero <elherzt@gmail.com>
2013-11-26 10:36:32 -0600

    added test to create event

---
+++ spec/features/create_new_event_spec.rb
@@ -0,0 +1,21 @@
+require "spec_helper"
+
+feature "creating a new event" do
+  before do
+    login
+    visit root_path
+  end
+  scenario "with valid attributes" do
+    click_link "New Event"
+    event =  attributes_for(:event) 
+    fill_in "event[name]", with: event.name
+    fill_in "event[start_date]", event.start_date
+    fill_in "event[end_date]", with: event.end_date
+    fill_in "event[place]", with: event.place
+    fill_in "event[cost]", with: event.cost
+    fill_in "event[requirements]", with: event.requirements
+    fill_in "event[notes]", with: event.notes
+  end
+
+end
+  
