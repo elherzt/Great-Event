5d9a46ffe2715ad71d8f603ec8e1aca00b6d651f
Jesus Romero <elherzt@gmail.com>
2013-11-26 16:37:08 -0600

    refactored to login with devise

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,10 @@
+
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+include Warden::Test::Helpers
+Warden.test_mode!
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
@@ -13,28 +16,14 @@ RSpec.configure do |config|
 
   config.treat_symbols_as_metadata_keys_with_true_values = true
 
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
   config.infer_base_class_for_anonymous_controllers = false
   config.order = "random"
-
-  #config.before(:suite) do
-    #DatabaseCleaner.strategy = :transaction
-  #end
-  #config.before(:each) do
-    #DatabaseCleaner.start
-  #end
-  #config.after(:each) do
-    #DatabaseCleaner.clean
-  #end
 end
 
 
-def login 
+def login
   user = create(:user)
-  user.save
-  visit '/users/sign_in'
-  fill_in "user[username]",    :with => user.username
-  fill_in "user[password]", :with => user.password
-  click_button "Sign in"
-  page.should have_content(user.username)
+  login_as(user, :scope => :user, :run_callbacks => false)
 end
+
