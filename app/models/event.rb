554cb9531b1e112ce65d362cff714262a0a04fc7
Jesus Romero <elherzt@gmail.com>
2013-11-26 18:23:05 -0600

    added vies to events aand users and datepicker js

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,16 @@
 class Event < ActiveRecord::Base
   belongs_to :user
   validates :user_id, :name, :description, :place, :hour, :start_date, presence: true
+  scope :after, -> { where "start_date > ?", Time.now }
+  scope :done, -> { where "end_date < ?", Time.now }
+  scope :next, -> { where "start_date >= ? AND start_date <= ?", from, to }
+
+  def self.from
+    Time.now
+  end
+
+  def self.to
+    Time.now + 30.days
+  end
+
 end
