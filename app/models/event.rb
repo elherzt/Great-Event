ddcc0bd46dc2430f861573ae335fe9aadc5cf4dc
Jesus Romero <elherzt@gmail.com>
2013-11-27 18:59:46 -0600

    refactor code and added style for show events menu

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,23 +1,22 @@
 class Event < ActiveRecord::Base
   belongs_to :user
   validates :user_id, :name, :description, :place, :hour, :start_date, presence: true
-  scope :after, -> { where "start_date > ?", Time.now }
+  scope :current, -> { where "start_date < ? AND end_date > ?", Time.now, Time.now }
   scope :done, -> { where "end_date < ?", Time.now }
   scope :next, -> { where "start_date >= ? AND start_date <= ?", from, to }
 
-  def first_date 
+  def start_date_format
     start_date.strftime("%d %B %Y")
   end
 
 
-  def second_date
+  def end_date_format
     if end_date.present?
       end_date.strftime("%d %B %Y")
     else
       start_date.strftime("%d %B %Y")
     end
   end
-
 
   def self.from
     Time.now
